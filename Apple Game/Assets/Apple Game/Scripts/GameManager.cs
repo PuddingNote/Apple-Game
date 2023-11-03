14618b4f01ddf86f0bfc88ba4c37739e7452f583
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-11-03 17:33:03 +0900

    Add Sample Setting
    1. Add Sample BackGround
    2. Add Sample Apple

---
+++ Apple Game/Assets/Apple Game/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void TouchDown()
+    {
+
+
+    }
+
+    public void TouchUp()
+    {
+
+    }
+
+
+
+
+}
